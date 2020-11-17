a3c7f8edba3255b269ec10cc908f1073f77501a0
vinhphan812 <vinhphan812@gmail.com>
2020-11-17 22:51:24 +0700

    fix menu

--- fuhlen.js
+++ fuhlen.js
@@ -1,19 +1,32 @@
-var index = 1;
-Next = function() {
-    var imgs = ["g900s.jpg", "phone.jpg"];
-    document.getElementById('img').src = imgs[index];
-    index++;
-    if (index == 2) {
-        index = 0;
-    }
-}
+window.onload = () => {
+     var index = 1;
+     Next = function() {
+          var imgs = ["g900s.jpg", "phone.jpg"];
+          document.getElementById('img').src = imgs[index];
+          index++;
+          if (index == 2) {
+               index = 0;
+          }
+     }
 
-Back = function() {
-    var imgs = ["g900s.jpg", "phone.jpg"];
-    document.getElementById('img').src = imgs[index];
-    index++;
-    if (index == 2) {
-        index = 0;
-    }
+     Back = function() {
+          var imgs = ["g900s.jpg", "phone.jpg"];
+          document.getElementById('img').src = imgs[index];
+          index++;
+          if (index == 2) {
+               index = 0;
+          }
+     }
+     setInterval(Next, 3000);
+
+     window.onscroll = showMenu;
+
+     function showMenu() {
+          var menu = document.getElementById('menu');
+          if (document.documentElement.scrollTop > 127)
+               menu.classList.add('nav_menu');
+          else
+               menu.classList.remove('nav_menu');
+          console.log("scroll: " + document.documentElement.scrollTop);
+     }
 }
-setInterval(Next, 3000);
