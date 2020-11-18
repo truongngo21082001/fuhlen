9241835ea79ed4f853869964a72b46f02cb0777d
Ngo Nhut Truong <19dh110998@st.huflit.edu.vn>
2020-11-19 06:33:57 +0700

    FUHLEN

--- fuhlen.js
+++ fuhlen.js
@@ -1,34 +1,61 @@
+const Imgs = ["hinh/g900s.jpg", "hinh/phone.jpg", ];
+var slide, wCarousel = 0,
+    wImg = 0,
+    posCarousel = 0;
+
 window.onload = () => {
-     var index = 1;
-     Next = function() {
-          var imgs = ["g900s.jpg", "phone.jpg"];
-          document.getElementById('img').src = imgs[index];
-          index++;
-          if (index == 2) {
-               index = 0;
-          }
-     }
-
-     Back = function() {
-          var imgs = ["g900s.jpg", "phone.jpg"];
-          document.getElementById('img').src = imgs[index];
-          index++;
-          if (index == 2) {
-               index = 0;
-          }
-     }
-     setInterval(Next, 3000);
-
-     showMenu();
-
-     window.onscroll = showMenu;
-
-     function showMenu() {
-          var menu = document.getElementById('menu');
-          if (document.documentElement.scrollTop > 127)
-               menu.classList.add('nav_menu');
-          else
-               menu.classList.remove('nav_menu');
-          console.log("scroll: " + document.documentElement.scrollTop);
-     }
+    slide = document.getElementById('slide');
+    showMenu();
+    renderCarousel();
+    window.onscroll = showMenu;
 }
+
+function renderCarousel() {
+    slide.innerHTML = Imgs.map((i) => {
+        return `<div class="carousel-item">
+            <img src="${i}" class="d-block w-100">
+        </div>`;
+    }).join('');
+    slide.children[0].classList.add('active');
+
+}
+
+function showMenu() {
+    var menu = document.getElementById('menu');
+    if (document.documentElement.scrollTop > 127)
+        menu.classList.add('nav_menu');
+    else
+        menu.classList.remove('nav_menu');
+}
+
+setInterval(showMenu, 3000);
+
+// (function() {
+//     ("#menu").click(function(e) {
+//         e.preventDefault();
+//         ("#wrapper").toggleClass("caret");
+//     });
+//     (window).resize(function(e) {
+//         if ((window).width() <= 768) {
+//             ("#wrapper").removeClass("caret");
+//         } else {
+//             ("#wrapper").addClass("caret");
+//         }
+//     })
+// })
+
+// const mainMenu = document.querySelector('.mainMenu');
+// const closeMenu = document.querySelector('.closeMenu');
+// const openMenu = document.querySelector('.openMenu');
+
+// openMenu.addEventListener('click', show);
+// closeMenu.addEventListener('click', close);
+
+// function show() {
+//     mainMenu.style.display = 'flex';
+//     mainMenu.style.top = '0';
+// }
+
+// function close() {
+//     mainMenu.style.top = '-100%';
+// }
