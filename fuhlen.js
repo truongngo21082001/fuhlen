d8b0ba70cd815e1c1b0eceae3baa67e0bd553bb1
Ngo Nhut Truong <19dh110998@st.huflit.edu.vn>
2020-11-23 21:44:43 +0700

    fixe product

--- fuhlen.js
+++ fuhlen.js
@@ -44,18 +44,18 @@ setInterval(showMenu, 3000);
 //     })
 // })
 
-// const mainMenu = document.querySelector('.mainMenu');
-// const closeMenu = document.querySelector('.closeMenu');
-// const openMenu = document.querySelector('.openMenu');
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 
-// openMenu.addEventListener('click', show);
-// closeMenu.addEventListener('click', close);
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
 
-// function show() {
-//     mainMenu.style.display = 'flex';
-//     mainMenu.style.top = '0';
-// }
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
 
-// function close() {
-//     mainMenu.style.top = '-100%';
-// }
+function close() {
+    mainMenu.style.top = '-100%';
+}
